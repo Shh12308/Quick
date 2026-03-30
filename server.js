43e68961cbba697573ab6b8b14651695dd04728c
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-03-31 00:50:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import pg from "pg";
-import { Worker as BullWorker } from "bullmq";
+import { Worker } from "bullmq";
 import { Worker as ThreadWorker } from "worker_threads";
 import argon2 from "argon2";
 import jwt from "jsonwebtoken";
