f8b617d7524e1473211c62e0ce00158f86d42a8c
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-05-03 14:47:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.use(helmet({
   contentSecurityPolicy: false // Disabled to allow flexibility with frontend images/scripts
 }));
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 // ==========================================
 // STRIPE WEBHOOK (Raw Body)
